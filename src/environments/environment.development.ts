7045e9ee0019511c2a296d11b4c524f194986c33
yoadwo <yoadwo@users.noreply.github.com>
2023-05-26 13:28:18 +0000

    Update gh-pages to output generated at c29b0ef

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    getGuestsInfo: 'https://yemz5f82ph.execute-api.us-east-1.amazonaws.com/dev',
-    textGuests: 'https://yemz5f82ph.execute-api.us-east-1.amazonaws.com/dev',
-};
--- src/environments/environment.production.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    getGuestsInfo: 'https://jrqqtm7tyh.execute-api.us-east-1.amazonaws.com/PROD',
-    textGuests: 'https://jrqqtm7tyh.execute-api.us-east-1.amazonaws.com/PROD',
-};
